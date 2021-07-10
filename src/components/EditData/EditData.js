38d3832e94d859ea25da92a7ead256c6435a4d93
Sehbaz Rafik <sehbazm@gmail.com>
2021-07-11 00:37:56 +0100

    edit form tested with raw data

--- src/components/EditData/EditData.js
+++ src/components/EditData/EditData.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../Common/Common.css";
 const EditData = () => {
-  return <div>Edit Data Component</div>;
+  return (
+    <div className="center-container">
+      Edit Data Component
+      <Link to="/">
+        {" "}
+        <button>Back</button>
+      </Link>
+      <div>
+        <form>
+          <input name="name" type="text" placeholder="name" /> <br />
+          <br />
+          <input name="price" type="number" placeholder="price" /> <br />
+          <br />
+          <input name="currency" type="text" placeholder="currency" /> <br />
+          <br />
+          <Link to="/">
+            {" "}
+            <button type="submit" value="Submit">
+              Submit
+            </button>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
 };
 export default EditData;
--- src/components/ShowTable/ShowTable.js
+++ src/components/ShowTable/ShowTable.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "../Common/Common.css";
 import { Link } from "react-router-dom";
 const ShowTable = () => {
   return (
