ca3c2791db043d795d6f083e81d14756a63db757
Sehbaz Rafik <sehbazm@gmail.com>
2021-07-11 00:32:52 +0100

    New data entry form created

--- src/components/AddData/AddData.js
+++ src/components/AddData/AddData.js
@@ -1,8 +1,31 @@
 import React from "react";
 import "../Common/Common.css";
+import { Link } from "react-router-dom";
 const AddData = () => {
   return (
-    <div className="center-container addData-container">Add Data Component</div>
+    <div className="center-container addData-container">
+      Add Data Component
+      <Link to="/">
+        {" "}
+        <button>Back</button>
+      </Link>
+      <div>
+        <form>
+          <input name="name" type="text" placeholder="name" /> <br />
+          <br />
+          <input name="price" type="number" placeholder="price" /> <br />
+          <br />
+          <input name="currency" type="text" placeholder="currency" /> <br />
+          <br />
+          <Link to="/">
+            {" "}
+            <button type="submit" value="Submit">
+              Submit
+            </button>
+          </Link>
+        </form>
+      </div>
+    </div>
   );
 };
 export default AddData;
