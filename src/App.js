0f839038ce818034ed8c7d2b6a54646c6685e287
Sehbaz Rafik <sehbazm@gmail.com>
2021-07-10 23:53:04 +0100

    Routing added to app file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
