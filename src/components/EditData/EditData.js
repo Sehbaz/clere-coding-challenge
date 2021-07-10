8e756df08ea7e783442c69f424dbf8869b8a5fd9
Sehbaz Rafik <sehbazm@gmail.com>
2021-07-11 00:53:47 +0100

    edit data form input tracker to variable

--- src/components/EditData/EditData.js
+++ src/components/EditData/EditData.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../Common/Common.css";
 const EditData = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [currency, setCurrency] = useState("");
+
+  const onInputNameChangeHandler = (e) => {
+    setName(e.target.value);
+  };
+  const onInputPriceChangeHandler = (e) => {
+    setPrice(e.target.value);
+  };
+  const onInputCurrencyChangeHandler = (e) => {
+    setCurrency(e.target.value);
+  };
+
+  const addEntry = (e) => {
+    e.preventDefault();
+    const newEntry = { name: name, price: parseInt(price), currency: currency };
+    console.log(newEntry);
+  };
   return (
     <div className="center-container">
       Edit Data Component
@@ -10,10 +29,23 @@ const EditData = () => {
         <button>Back</button>
       </Link>
       <div>
-        <form>
-          <input name="name" type="text" placeholder="name" required /> <br />
+        <form onSubmit={addEntry}>
+          <input
+            name="name"
+            type="text"
+            placeholder="name"
+            required
+            onChange={onInputNameChangeHandler}
+          />{" "}
           <br />
-          <input name="price" type="number" placeholder="price" required />{" "}
+          <br />
+          <input
+            name="price"
+            type="number"
+            placeholder="price"
+            required
+            onChange={onInputPriceChangeHandler}
+          />{" "}
           <br />
           <br />
           <input
@@ -21,6 +53,7 @@ const EditData = () => {
             type="text"
             placeholder="currency"
             required
+            onChange={onInputCurrencyChangeHandler}
           />{" "}
           <br />
           <br />
