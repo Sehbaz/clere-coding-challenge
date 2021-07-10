e3ce953fe56fedbf86f8e43cac18e922f5ebb767
Sehbaz Rafik <sehbazm@gmail.com>
2021-07-11 00:59:24 +0100

    Get all data from table, connected to api using axios

--- src/components/ShowTable/ShowTable.js
+++ src/components/ShowTable/ShowTable.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../Common/Common.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
 const ShowTable = () => {
+  const [data, setData] = useState([]);
+  const getAllData = () => {
+    axios.get(`https://test.clerenet.com/product`).then((result) => {
+      setData(result.data);
+    });
+  };
+
+  useEffect(() => {
+    getAllData();
+  }, []);
   return (
     <div>
       Show table component
@@ -14,32 +25,24 @@ const ShowTable = () => {
               <td>Price</td>
               <td>Action</td>
             </tr>
-            <tr>
-              <td>1</td>
-              <td>Sehbaz Rafik</td>
-              <td>10 GBP</td>
-              <td>
-                {" "}
-                <Link to="/editData">
+            {data.map((row) => (
+              <tr key={row.id}>
+                <td>{row.id}</td>
+                <td>{row.name}</td>
+                <td>
+                  {row.price}
+                  {row.currency}
+                </td>
+                <td>
                   {" "}
-                  <button>Edit</button>
-                </Link>
-                <button>DELETE</button>
-              </td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>Sehbaz Faiz</td>
-              <td>10 GBP</td>
-              <td>
-                {" "}
-                <Link to="/editData">
-                  {" "}
-                  <button>Edit</button>
-                </Link>{" "}
-                <button>DELETE</button>
-              </td>
-            </tr>
+                  <Link to="/editData">
+                    {" "}
+                    <button>Edit</button>
+                  </Link>
+                  <button>DELETE</button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
