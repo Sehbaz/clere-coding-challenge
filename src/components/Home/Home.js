b2ccc07ab6f0ef86f8ce29d4b773991a3c994624
Sehbaz Rafik <sehbazm@gmail.com>
2021-07-14 07:34:03 +0100

    Material UI table added with fetched data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,12 +6,19 @@ import ShowTable from "../ShowTable/ShowTable";
 const Home = () => {
   return (
     <div className="home-container center-container">
-      Home Component
-      <Link to="/addData">
-        {" "}
-        <button>ADD</button>
-      </Link>
       <div>
+        {" "}
+        <h1 className="title">Product Details </h1>
+      </div>
+      <div>
+        {" "}
+        <Link to="/addData">
+          {" "}
+          <button className="submit-btn">ADD</button>
+        </Link>
+      </div>
+
+      <div className="table-container">
         <ShowTable />
       </div>
     </div>
--- src/components/ShowTable/ShowTable.js
+++ src/components/ShowTable/ShowTable.js
@@ -3,9 +3,110 @@ import "../Common/Common.css";
 import "./ShowTable.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
-
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { Button } from "@material-ui/core";
+import PropTypes from "prop-types";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import FirstPageIcon from "@material-ui/icons/FirstPage";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import LastPageIcon from "@material-ui/icons/LastPage";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableFooter from "@material-ui/core/TableFooter";
 //Default API endpoint
 axios.defaults.baseURL = "https://test.clerenet.com/product";
+const useStyles1 = makeStyles((theme) => ({
+  root: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(2.5),
+  },
+}));
+
+function TablePaginationActions(props) {
+  const classes = useStyles1();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
+function createData(name, calories, fat) {
+  return { name, calories, fat };
+}
+const useStyles2 = makeStyles({
+  table: {
+    minWidth: 500,
+  },
+});
 
 //Showing all the records
 const ShowTable = () => {
@@ -65,15 +166,161 @@ const ShowTable = () => {
       .delete("https://test.clerenet.com/product/" + e)
       .then(() => getAllData());
   };
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  const classes = useStyles2();
 
   return (
-    <div>
-      Show table component
+    <div className="main-container">
       <div style={{ display: noDataIndicator }}>
         {" "}
         No Data in database recieved
       </div>
       <div style={{ display: dataIndicator }}>
+        <TableContainer component={Paper}>
+          <Table
+            className={classes.table}
+            aria-label="custom pagination table"
+            size="small"
+          >
+            <TableHead>
+              <TableRow>
+                <TableCell align="left">ID</TableCell>
+                <TableCell align="left" component="th" scope="row">
+                  Name
+                </TableCell>
+                <TableCell align="left">Price</TableCell>
+                <TableCell align="center">Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {(rowsPerPage > 0
+                ? data.slice(
+                    page * rowsPerPage,
+                    page * rowsPerPage + rowsPerPage
+                  )
+                : data
+              ).map((row) => (
+                <TableRow key={row.id}>
+                  <TableCell>{row.id}</TableCell>
+                  <TableCell align="left">{row.name}</TableCell>
+                  <TableCell align="left">
+                    {row.price} {row.currency}
+                  </TableCell>
+                  <TableCell align="center">
+                    <Link
+                      to={{
+                        pathname: "/editData",
+                        state: {
+                          productId: row.id,
+                        },
+                      }}
+                    >
+                      {" "}
+                      <Button variant="outlined" color="primary">
+                        Edit
+                      </Button>
+                    </Link>
+
+                    <Button
+                      variant="outlined"
+                      color="secondary"
+                      onClick={deleteRecord.bind(this, row.id)}
+                    >
+                      Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                  colSpan={3}
+                  count={data.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  SelectProps={{
+                    inputProps: { "aria-label": "rows per page" },
+                    native: true,
+                  }}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActions}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+        {/*
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">ID</TableCell>
+                <TableCell align="center">Name</TableCell>
+                <TableCell align="center">Price</TableCell>
+                <TableCell align="center">Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((row) => (
+                <TableRow key={row.id}>
+                  <TableCell align="center">{row.id}</TableCell>
+                  <TableCell component="th" scope="row" align="center">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="center">
+                    {row.price} {row.currency}
+                  </TableCell>
+                  <TableCell align="center">
+                    <Link
+                      to={{
+                        pathname: "/editData",
+                        state: {
+                          productId: row.id,
+                        },
+                      }}
+                    >
+                      {" "}
+                      <Button variant="outlined" color="primary">
+                        Edit
+                      </Button>
+                    </Link>
+
+                    <Button
+                      variant="outlined"
+                      color="secondary"
+                      onClick={deleteRecord.bind(this, row.id)}
+                    >
+                      Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+       
         <table>
           <tbody>
             <tr>
@@ -111,6 +358,7 @@ const ShowTable = () => {
             ))}
           </tbody>
         </table>
+                  */}
       </div>
     </div>
   );
